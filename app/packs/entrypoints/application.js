8978a818dfe1941ef12aba71c16d804593ea57e5
Josh <jleblanc@omics.com>
2021-06-10 13:02:06 -0300

    vce

--- app/packs/entrypoints/application.js
+++ app/packs/entrypoints/application.js
@@ -3,11 +3,29 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+import jquery from 'jquery';
+
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+
+window.$ = jquery;
+window.jQuery = jquery;
+
+import "pikaday/plugins/pikaday.jquery";
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+import "controllers"
+
+$(document).on("turbolinks:load", function() {
+    $('.ui.dropdown').dropdown();
+    $('.menu .item').tab();
+    $('.datepicker').pikaday();
+    $.ajaxSetup({ cache: false });
+});
+
